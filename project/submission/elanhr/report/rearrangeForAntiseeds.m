230a6d2a6eef784532ce5dbbc80aefb848e52599
Elan Hourticolon-Retzler <mail@elanhr.org>
2015-05-14 23:53:08 -0400

    adding my final project to the repo

---
+++ project/submission/elanhr/report/rearrangeForAntiseeds.m
@@ -0,0 +1,13 @@
+function [ output_args ] = rearrangeForAntiseeds( A )
+%REARRANGEFORANTISEEDS Changes order for antiseeds
+%block matrix
+%   Detailed explanation goes here
+
+B = cumsum(A); 
+
+%find 
+
+sum(B == 0) % number of 
+
+sort(A)
+
