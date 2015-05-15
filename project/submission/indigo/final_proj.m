228e1edb37076590684e95293a2bdcea68ff0c36
Indigo Rose <4b0082rose@gmail.com>
2015-05-14 23:16:23 -0400

    Upload Final Project

--- project/submission/indigo/final_proj.m
+++ project/submission/indigo/final_proj.m
@@ -7,7 +7,7 @@
 clear; close all; clc;
 
 %% Download Data
-
+%{
 %Download & unzip data using wget, as specified by Greg
 system('wget https://www.dropbox.com/sh/idt3d0gylplyo31/AADfoAAgcWg-vq0SsRpx1u_la/Indigo.zip?dl=1');
 system('unzip Indigo.zip*');
@@ -15,7 +15,7 @@ system('unzip Indigo.zip*');
 %Remove extra files from unzipping
 system('rm Indigo.zip?dl=1');
 system('rmdir __MACOSX');
-
+%}
 %% Import Data
 ant_raw = csvread('out-ant.csv',1,1,[1,1,49,49]); % ant = anteriorgrade tracer data
 ret_raw = csvread('out-ret.csv',1,1,[1,1,49,49]); % ret = retrograde tracer data
@@ -31,10 +31,10 @@ lab_withPHAL = [lab_in{:, 1}];
 lab = lab_withPHAL(2:end); %Excludes title line "PHAL"
 
 %Import groundtruth data
-gt = csvread('Groundtruth.csv',1,1,[1,1,49,3]);
+%gt = csvread('Groundtruth.csv',1,1,[1,1,49,3]);
 
 %% Plot Unclustered Data
-figure(1);
+figure(1); %subplot(1,2,1);
 imagesc(ant); colorbar('FontSize',11);
 title('Unclustered Anterograde Transport','FontSize',16);
 ylabel('Cortical Area, Connecting From:','FontSize',14);
@@ -43,7 +43,7 @@ set(gca, 'XTick', 1:49, 'XTickLabel', lab,'FontSize',8);
 set(gca, 'YTick', 1:49, 'YTickLabel', lab,'FontSize',8);
 rotateXLabels(gca(),90);
 
-figure(2);
+figure(2); %subplot(1,2,2);
 imagesc(ret); colorbar('FontSize',11);                    
 title('Unclustered Retrograde Transport','FontSize',16);
 ylabel('Cortical Area, Connecting From:','FontSize',14);
@@ -65,13 +65,13 @@ rotateXLabels(gca(),90);
 % Complile matricies into cmat. This has the same orientation as ant, so
 % the regions listed in the rows map onto regions listed in the columns
 
-cmat_unthresh = ant + ret'; %unthresholded
+cmat = ant + ret'; %unthresholded
 
 % Threshold: Axon tracts that don't match back between ant and ret are set 
 % to 0. This was specified in the extra material portion of the paper.
 
 threshold = 2; %Anything <2 gets set to 0
-cmat_thresh = threshold_absolute(cmat_unthresh,threshold);
+cmat_thresh = threshold_absolute(cmat,threshold);
 
 cmat_bin = weight_conversion(cmat_thresh, 'binarize'); 
 %Talk about the implications of doing this in report
@@ -90,9 +90,17 @@ labsort12 = sortcell(lab12,3);
 lab4 = [lab num2cell(index') num2cell(clust_kmeans4)];
 labsort4 = sortcell(lab4,3);
 
-figure;
+figure(3);
 %plot(labsort4(2),labsort4(3));
+subplot(2,1,1);
 bar(cell2mat(labsort12(:,2)),cell2mat(labsort12(:,3)));
+title('kmeans, k=12','FontSize',16);
+xlabel('Brain Region Number','FontSize',14);
+
+subplot(2,1,2);
+bar(cell2mat(labsort4(:,2)),cell2mat(labsort4(:,3)));
+title('kmeans, k=4','FontSize',16);
+xlabel('Brain Region Number','FontSize',14);
 
 %labn = cell2mat(lab);
 %test = 1:49;
@@ -115,10 +123,17 @@ sortout(:,2)
 %}
 
 
-figure(3);
-silhouette(cmat_bin,clust_kmeans4);
 figure(4);
+subplot(1,2,1);
+silhouette(cmat_bin,clust_kmeans4);
+title('kmeans, k=4','FontSize',16);
+ylabel('Cluster','FontSize',14);
+
+%figure(4);
+subplot(1,2,2);
 silhouette(cmat_bin,clust_kmeans12);
+title('kmeans, k=12','FontSize',16);
+ylabel('Cluster','FontSize',14);
 
 
 %% Heirarchical Clustering Algorithm 
@@ -131,20 +146,40 @@ linka = linkage(dist,'average');
 linkw = linkage(dist,'weighted');
 
 %figure; dendrogram(links);
-figure(5); dendrogram(linkc);
+figure(5); 
+subplot(2,2,1);
+dendrogram(links);
+title('Dendrogram of Clusters Under Simple Linkage Method','FontSize',9);
+xlabel('Neocortical Region Number');
+
+subplot(2,2,2);
+dendrogram(linkc);
+title('Dendrogram of Clusters Under Complete Linkage Method','FontSize',9);
+xlabel('Neocortical Region Number');
 %set(gca, 'XTick', 1:49, 'XTickLabel', lab,'FontSize',8);
 %rotateXLabels(gca(),90);
 
-figure(6); dendrogram(linka); %These are being super weird
-figure(7); dendrogram(linkw);
+subplot(2,2,3);
+dendrogram(linka); %These are being super weird
+title('Dendrogram of Clusters Under Average Linkage Method','FontSize',9);
+xlabel('Neocortical Region Number');
+
+subplot(2,2,4);
+dendrogram(linkw);
+title('Dendrogram of Clusters Under Weighted Linkage Method','FontSize',9);
+xlabel('Neocortical Region Number');
 
 
+%% Rearrange Matrix
 
-%% Spectral Cluster Algorithm
-
-% :(
-
-%% Something else
+figure(6);
+[order1 cmatsort]= reorder_mod(cmat,clust_kmeans12');
+imagesc(cmatsort); colorbar('FontSize',11); 
+xlabel('Brain Region Number','FontSize',14);
+ylabel('Brain Region Number','FontSize',14);
+set(gca, 'XTick', 1:49, 'XTickLabel', order1,'FontSize',8);
+set(gca, 'YTick', 1:49, 'YTickLabel', order1,'FontSize',8);
+title('Ordered Combined Matrix','FontSize',16);
 %{
 cmat = cmat_bin + cmat_bin';
 r = symrcm(cmat);
